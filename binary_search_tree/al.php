6c397f50dc036694fb6bfacf24cf472228625f2a
nagomi <nagomi.github@gmail.com>
2017-03-07 14:18:10 +0900

    update

--- binary_search_tree/al.php
+++ binary_search_tree/al.php
@@ -4,68 +4,64 @@ class Tree{
 	private $direction;
 	private $value;
 	private $cnt = 0;
+	private $update_index = 0;
+	private $depth = 0;
 	public function __construct($value){
-		$this->root = new Node($value);
+		$option = new stdClass();
+		$option->index = $this->update_index++;
+		$option->parent_direction = 'none';
+		$option->depth = $this->depth;
+		$this->root = new Node($value,$option); 
 		$this->root->left(null);
 		$this->root->right(null);
 	}
 	public function insert($value){
+		$this->depth = 1;
 		$this->search($this->root, $value);
 	}
 
 
-	public function flow(){
-		$node = $this->root;
-	}
 	public function search(Node $node, $value){
 		$this->cnt++;
-		if($this->cnt > 5)exit("end");
-		echo "search:$value" . PHP_EOL;
-		echo "node->value:" . $node->value() . PHP_EOL;
+		if($this->cnt > 100)exit("end");
 		if($this->whichIsNull($node)){
 			echo "update:$value" . PHP_EOL;
 			return $this->updateEitherDirectionNullNode($node, $value);
 		}
-//		var_dump($node->left());
 		if($node->left()->value() < $value){
 			$nextNode = $node->left();
 		}else{
 			$nextNode = $node->right();
 		}
-//		var_dump($value);
-//		var_dump($nextNode);
-//		var_dump($nextNode->left());
-//		var_dump($nextNode->right());
-		echo "before search" . PHP_EOL;
+		$this->depth++;
 		$this->search($nextNode, $value);
 	}
 	public function whichIsNull($node){
 		return $node->left() === null || $node->right() === null;
 	}
 	public function updateEitherDirectionNullNode($node, $value){
-		$newNode = new Node($value);
+		$option = new stdClass();
+		$option->index = $this->update_index++;
+		$option->depth = $this->depth;
 		if($node->left() === null){
-			echo "left -> ";
-			//var_dump($newNode);
-			return $node->left($newNode);
+			$option->parent_direction = 'left';
+			return $node->left(new Node($value, $option));
 		}
 		if($node->right() !== null){
 			throw new Exception("Either one must be null.");
 		}
 		if($node->left()->value() <= $value){
-			echo "right -> ";
-			//var_dump($newNode);
-			return $node->right($newNode);
+			$option->parent_direction = 'right';
+			return $node->right(new Node($value, $option));
 		}else{
 			echo "-------------------------" . PHP_EOL;
+			$option->parent_direction = 'left';
 			$left = $node->left();
-			$node->left($newNode);
+			$node->left(new Node($value, $option));
 			return $node->right($left);
 		}
 	}
 
-	public function find(){
-	}
 	public function each($callback, $node=null){
 		$node = $node === null ? $this->root:$node;
 		if($node->left()){
@@ -79,7 +75,7 @@ class Tree{
 	}
 	public function display(){
 		$this->each(function(Node $node){
-			echo "" . $node->value() . PHP_EOL;
+			echo $node->index . ":" . $node->value() . ' -> ' . $node->depth . '=' . $node->parent_direction . PHP_EOL;
 		}, null);
 	}
 }
@@ -87,11 +83,17 @@ class Node{
 	public $value;
 	public $leftNode;
 	public $rightNode;
+	public $index;
+	public $parent_direction;
+	public $depth;
 
-	public function __construct($value){
+	public function __construct($value, stdClass $option){
 		$this->value = $value;
 		$this->leftNode = null;
 		$this->rightNode = null;
+		$this->index = $option->index;
+		$this->parent_direction = $option->parent_direction;
+		$this->depth = $option->depth;
 	}
 	public function left(Node $node = null){
 		if($node ===  null){
@@ -117,11 +119,11 @@ $tree = new Tree(50);
 $tree->insert(9);
 $tree->insert(60);
 $tree->insert(12);
-//$tree->insert(5);
-//$tree->insert(15);
-//$tree->insert(8);
-//$tree->insert(2);
-//$tree->insert(6);
+$tree->insert(5);
+$tree->insert(15);
+$tree->insert(8);
+$tree->insert(2);
+$tree->insert(6);
 //
 $tree->display();
 
